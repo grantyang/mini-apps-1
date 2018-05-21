19455834e042f55e908d32f2fa4f34a2cdd091db
Grant Yang <grantyang1@gmail.com>
2018-05-21 12:06:14 -0700

    ability to place alternating Xs and Os complete

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,29 @@
+document.addEventListener('DOMContentLoaded', function(event) {
+  console.log('loaded');
+  var currentPlayer = 'x';
+  var resetBtn = document.getElementById('reset');
+  resetBtn.addEventListener('click', e => {
+    console.log('reset clicked');
+  });
+  var tiles = document.getElementsByClassName('tile');
+  for (let tile of tiles) {
+    console.log(tile);
+    tile.addEventListener('click', e => {
+      onTileClick(tile);
+    });
+  }
 
+  var onTileClick = function(tile) {
+    if (tile.innerHTML === '[_]'){
+      tile.innerHTML = `[${currentPlayer}]`;
+      currentPlayer = currentPlayer === 'x' ? 'o' : 'x';
+    } else {
+    console.log('tile already taken', tile);
+      
+    }
 
+    console.log('tile clicked', tile);
+  };
+
+  var onResetClick = function() {};
+});
