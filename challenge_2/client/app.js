bfd651c6abe325735a625d944cfae6be41320fb5
Grant Yang <grantyang1@gmail.com>
2018-05-22 16:47:54 -0700

    csv converter

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,22 @@
+window.addEventListener('DOMContentLoaded', function(){
+  $('#submitBtn').on('click', (event)=>{
+    event.preventDefault()
+    postJSON($('#jsonInput').val())
+  })
+})
 
-
+var postJSON = function(input){
+  $.ajax({
+    type: 'POST',
+    url: 'http://127.0.0.1:3000/json',
+    contentType: 'application/json',
+    data: input,
+    success: (responseData) => {
+      console.log('success', responseData)
+      $('#result').html(responseData)
+    },
+    error: (error) => {
+      console.log('there was an error', error)
+    }
+  })
+}
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,59 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
 
+const app = express();
+app.use(express.static('client'));
+app.use(bodyParser.json());
 
+app.post('/json', (req, res) => {
+  let json = req.body;
+  res.send(generateCSV(json));
+});
+
+var addRowId = function(newLine, rowCount) {
+  newLine[0] = rowCount;
+};
+var addRowParentId = function(newLine, parentId) {
+  newLine[1] = parentId;
+};
+
+var generateCSV = function(initialJSON) {
+  let rowCount = 0;
+  let rowId = 0;
+  let firstString = '';
+  let bodyString = '';
+  let firstLine = ['rowId', 'parentId'];
+
+  var recurse = function(jsonObj, parentId) {
+    let newLine = [null, null];
+    for (let field in jsonObj) {
+      if (bodyString === '' && field !== 'children') {
+        firstLine.push(field);
+      }
+      if (field !== 'children') {
+        newLine.push(jsonObj[field]);
+      }
+    }
+    
+    addRowId(newLine, rowCount);
+    addRowParentId(newLine, parentId);
+    let savedId = rowCount;
+    rowCount++;
+
+    bodyString = bodyString.concat(newLine.join(',') + '<br>');
+    console.log('rowId is', rowId)
+    console.log(newLine.join(',') + '<br>')
+    console.log('children of this row are', jsonObj.children)
+    for (let child of jsonObj.children) {
+      console.log('passing in', savedId)
+      recurse(child, savedId);
+    }
+  };
+
+  recurse(initialJSON, 0);
+  firstString = firstLine.join(',') + '<br>';
+  return firstString.concat(bodyString);
+};
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
